4803dfabc5757d8a0bfe57089e0d75dee84c1451
John David Wheeler <jdw0147@auburn.edu>
2025-09-16 15:28:18 -0500

    Created official download method

--- YoutubeMp3.py
+++ YoutubeMp3.py
@@ -1,8 +1,19 @@
 # Imports
 import os
 import subprocess # To run ffmpeg
-from yt_dlp import YoutubeDl as Ytdl # To download YouTube Audio
+from yt_dlp import YoutubeDl # To download YouTube Audio
 from mutagen.mp3 import MP3 # To edit MP3 metadata
 from mutagen.id3 import ID3, TIT2, TPE1, TALB, TDRC, TCON, APIC # ID3 tag types
 
 # Downloading a Youtube video from a link
+def download_audio(youtube_url, output_filename="temp.m4a"):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': output_filename,
+        'quiet': False,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+            'preferredquality': '192',
+        }]
+    }
