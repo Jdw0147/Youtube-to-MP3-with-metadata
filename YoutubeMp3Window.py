2d3573d70aad234af8c73b6d910125ea07a5a28d
John David Wheeler <jdw0147@auburn.edu>
2025-09-18 13:55:24 -0500

    Initialized dictionary of fields

--- YoutubeMp3Window.py
+++ YoutubeMp3Window.py
@@ -15,3 +15,27 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 from YoutubeMp3 import download_youtube_audio, to_mp3, add_metadata
+
+class YoutubeMp3Window(QWidget):
+    """
+    Main window for the YouTube → MP3 converter app using PySide6 (Qt).
+    In Qt, every visible window is a QWidget (or subclass of QWidget).
+    """
+
+    def __init__(self):
+        super().__init__()  # Initializing the base (parent) QWidget class
+        self.setWindowTitle("Convert Youtube to MP3")   # Setting window title
+
+        # INPUT FIELDS
+        # Remember QLineEdit is single line input, QTextEdit is multi-line input
+        # Defining as a dictionary so we can loop over it, creating the form easily
+        self.fields = {
+            "YouTube URL": QLineEdit(),
+            "Title": QLineEdit(),
+            "Artist": QLineEdit(),
+            "Album": QLineEdit(),
+            "Year": QLineEdit(),
+            "Genre": QLineEdit(),
+            "Track Number": QLineEdit(),
+            "Lyrics": QTextEdit(),
+        }
