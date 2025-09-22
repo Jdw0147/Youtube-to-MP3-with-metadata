fbc42dcf906c233c20877bec9bf6809e36074265
John David Wheeler <jdw0147@auburn.edu>
2025-09-22 07:15:19 -0500

    Removed YtMp3App.py
    Was originally going to be the Qt file but ended up using YoutubeMp3Window.py

--- YtMp3App.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from PySide6.QtWidgets import (
-    QApplication, QWidget, QLineEdit, QTextEdit, QPushButton, QLabel,
-    QFileDialog, QVBoxLayout, QHBoxLayout, QFormLayout, QMessageBox
-)
-from YoutubeMp3 import download_youtube_audio, to_mp3, add_metadata
