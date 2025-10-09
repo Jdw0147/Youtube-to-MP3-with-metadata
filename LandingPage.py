4cefd6359774cc6ee64ad1e4e5c564fa9bc4983f
John David Wheeler <jdw0147@auburn.edu>
2025-10-09 13:31:28 -0500

    Added self method

--- LandingPage.py
+++ LandingPage.py
@@ -3,3 +3,19 @@ from PySide6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget
 )
 from YoutubeMp3Window import YoutubeMp3Window
+
+class LandingPage(QWidget):
+    def __init__(self, switch_to_song, switch_to_album,):
+        super().__init__()
+        layout = QVBoxLayout()
+        label = QLabel("YouTube to MP3 Converter")
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+        btn_song = QPushButton("Convert Song")
+        btn_album = QPushButton("Convert Album/Playlist")
+        btn_song.clicked.connect(switch_to_song)
+        btn_album.clicked.connect(switch_to_album)
+        layout.addWidget(btn_song)
+        layout.addWidget(btn_album)
+        self.setLayout(layout)
+
